b8d26cbeba5f096bf025b845eaf9a3d4949757a5
고나현 <hy32214@naver.com'git config --global user.name >
2026-02-23 08:34:13 +0900

    0222

--- 과목평가3.py
+++ 과목평가3.py
@@ -1,3 +1,4 @@
+# 강사님 ver.
 import math
 
 balls = [
@@ -19,7 +20,40 @@ for i in range(1, 6):
         targetBall_x = balls[i][1]
         break
 
-math.atan2(targetBall_y-whiteBall_y, targetBall_x-whiteBall_x)
+radian = math.atan2(targetBall_y-whiteBall_y, targetBall_x-whiteBall_x)
 angle = math.degrees(radian)
 power = 100
 print(angle)
+
+
+# 누군가 ver.
+from math import atan2
+import math
+
+
+balls = [
+    [1, 2], # white_ball
+    [-1, -1],
+    [124, 565], # target_ball
+    [-1, -1],
+    [-1, -1],
+    [-1, -1]
+]
+# 위의 balls는 뭐 어떻게 알아서 나와있을듯
+
+
+white_ball = balls[0] # = [1, 2]
+
+for i in range(1, len(balls)):
+    if balls[i][0] >= 0:
+        target2_ball = balls[i] # = [124, 565]
+
+radian = atan2(target2_ball[0] - white_ball[0], target2_ball[1] - white_ball[1])
+
+angle = 180 / math.pi * radian
+
+power = 100
+
+'''
+angle , power 설정하기
+'''
